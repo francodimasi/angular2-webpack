f95f72988d4001d75be4a7cd671bc70a3415e272
epizmaragdis <zmara@ar.ibm.com>
2016-11-30 10:47:37 -0300

    cambio look and feel

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
-import { ProfileComponent } from './profile';
+
 
 
 import { DataResolver } from './app.resolver';
@@ -10,7 +10,6 @@ import { DataResolver } from './app.resolver';
 export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
   { path: '**',    component: HomeComponent }
 
 ];
