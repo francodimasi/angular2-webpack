84d8f0d69839faff0347fab2c5061f3dbfbb999a
francodimasi <francodimasi@hotmail.com>
2016-12-02 15:35:04 -0300

    add CORS

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.use(logger('dev'));
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
